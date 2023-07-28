93afe28890b4f2ce0704ef696eb47382bce6f18b
zngkgk963 <zngkgk963@gmail.com>
2023-07-28 14:34:00 +0900

    집에서 추가

---
+++ src/main/java/com/memo2/user/UserController.java
@@ -0,0 +1,12 @@
+package com.memo2.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+	
+	
+	
+}
