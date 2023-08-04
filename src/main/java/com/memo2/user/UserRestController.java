52cb6ddde42d083c6e20f3956188610ab952e0cf
test <test@gamil.com>
2023-08-04 18:41:00 +0900

    no message

--- src/main/java/com/memo2/user/UserRestController.java
+++ src/main/java/com/memo2/user/UserRestController.java
@@ -1,5 +1,6 @@
 package com.memo2.user;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.memo2.user.bo.UserBO;
+import com.memo2.user.entitiy.UserEntity;
 
 @RequestMapping("/user")
 @RestController
@@ -20,6 +22,13 @@ public class UserRestController {
 	@RequestMapping("/is_duplicated_id")
 	public Map<String, Object> isDuplicatedId(
 			@RequestParam("loginId") String loginId) {
+			
+		Map<String, Object> result = new HashMap<>();
+		result.put("isDuplicatedId", false);
+		
+		UserEntity userEntity = userBO.getUserEntityByLoginId(loginId);
+		result.put("code", 1);
+		
 		
 	}
 	
