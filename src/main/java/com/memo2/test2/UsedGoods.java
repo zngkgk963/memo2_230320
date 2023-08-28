4170da625d901e4148d5ed59caa5fe0369e686b0
test <test@gamil.com>
2023-08-28 18:41:18 +0900

    학원

---
+++ src/main/java/com/memo2/test2/UsedGoods.java
@@ -0,0 +1,48 @@
+package com.memo2.test2;
+
+public class UsedGoods {
+	private int id;
+	private String title;
+	private int price;
+	private String description;
+	private String picture;
+	private int sellerId;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPicture() {
+		return picture;
+	}
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+	public int getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(int sellerId) {
+		this.sellerId = sellerId;
+	}
+	
+}
